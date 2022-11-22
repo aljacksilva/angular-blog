a99afd31ed90576858e453085e289659fc57a0d1
Jackson <kinho160@hotmail.com>
2022-11-21 22:53:57 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+    {
+        "id":"1",
+        "title":"Saiu novo filme",
+        "description":"ola",
+        "photoCover":"https://www.magazine-hd.com/apps/wp/wp-content/uploads/2022/09/iron-man-ea.jpg"
+    },
+    {
+        "id":"2",
+        "title":"saiu novo filme",
+        "description":"ola",
+        "photoCover":""
+    }
+]
